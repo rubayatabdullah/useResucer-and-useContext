1b95a0866a302a8f874c13400ee02c8716b33a7d
rubayat <rubayatalabdullah@gmail.com>
2024-11-24 17:52:16 +0600

    initail app

---
+++ src/data/initialTasks.js
@@ -0,0 +1,17 @@
+export const initialTasks = [
+  {
+    id: 1,
+    title: "Learn HTML",
+    done: true,
+  },
+  {
+    id: 2,
+    title: "Learn CSS",
+    done: true,
+  },
+  {
+    id: 3,
+    title: "Learn JavaScript",
+    done: true,
+  },
+];
